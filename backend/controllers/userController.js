3e218dddfe5b1aadce031a581d8ccaf8694f607d
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-17 10:59:24 +0530

    Create user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,37 +1,61 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel";
 
 // @desc    Auth user/set token
 // route    POST /api/users/auth
 // @access  Public
-const authUser = asyncHandler((req, res) => {
+const authUser = asyncHandler(async (req, res) => {
 	res.status(200).json({ msg: "user Auth" });
 });
 
 // @desc    Register a new user
 // route    POST /api/users
 // @access  Public
-const registerUser = asyncHandler((req, res) => {
-	res.status(200).json({ msg: "user Registered" });
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	const userExists = await User.findOne({ email });
+
+	if (userExists) {
+		res.status(400);
+		throw new Error("User already exists");
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password,
+	});
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid user data");
+	}
 });
 
 // @desc    Logout user
 // route    POST /api/users/logout
 // @access  Public
-const logoutUser = asyncHandler((req, res) => {
+const logoutUser = asyncHandler(async (req, res) => {
 	res.status(200).json({ msg: "Logout User" });
 });
 
 // @desc    Get user profule
 // route    GET /api/users/profile
 // @access  Private
-const getUserProfile = asyncHandler((req, res) => {
+const getUserProfile = asyncHandler(async (req, res) => {
 	res.status(200).json({ msg: "User Profile" });
 });
 
 // @desc    Update user profule
 // route    PUT /api/users/profile
 // @access  Private
-const updateUserProfile = asyncHandler((req, res) => {
+const updateUserProfile = asyncHandler(async (req, res) => {
 	res.status(200).json({ msg: "User Profile updated" });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 4000;
 connectDB();
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is Ready!"));
