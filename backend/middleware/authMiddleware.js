4a116fb18473dba572d7aaafab17a16311cb0e3a
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-18 10:09:40 +0530

    Fix auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,7 +9,9 @@ const protect = asyncHandler(async (req, res, next) => {
 	if (token) {
 		try {
 			const decoded = jwt.verify(token, process.env.JWT_SECRET);
-			req.user = await User.findById(decoded.userId.select(-password));
+			req.user = await User.findById(decoded.userId).select("-password");
+
+			next();
 		} catch (err) {
 			res.status(401);
 			throw new Error("Not authorized, invalid token!");
