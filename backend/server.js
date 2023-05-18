6acac28d814dc566af6a3b8e242341564b28e746
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-18 10:01:58 +0530

    Auth middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 
 const port = 5000;
 // const port = process.env.PORT || 5000;
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
