2c4cbfa8a20e7c1ba7d3198eeb1aca92a4368992
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-22 15:43:10 +0530

    ready for production

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const port = 5000;
 // const port = process.env.PORT || 5000;
@@ -18,7 +19,19 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => res.send("Server is Ready!"));
+if (process.env.NODE_ENV === "production") {
+	const __dirname = path.resolve();
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+	);
+} else {
+	app.get("/", (req, res) => {
+		res.send("API is running....");
+	});
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
